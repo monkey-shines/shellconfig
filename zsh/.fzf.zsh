40d5a39ea4c51814a57b8e6b8d55c0199293125e
monkey-shines <58019324+monkey-shines@users.noreply.github.com>
2026-04-15 18:56:34 -0500

    Create .fzf.zsh

---
+++ zsh/.fzf.zsh
@@ -0,0 +1,2 @@
+[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
+[ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
