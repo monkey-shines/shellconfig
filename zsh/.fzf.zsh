98dd67a971dee6b00312face303dbd0c1dc2060a
monkey-shines <58019324+monkey-shines@users.noreply.github.com>
2026-04-15 20:11:48 -0500

    Delete zsh/.fzf.zsh

--- zsh/.fzf.zsh
+++
@@ -1,2 +0,0 @@
-[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
-[ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
